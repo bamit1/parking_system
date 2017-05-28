5153befe515f393c43a32a2f0e150eb55e5ea6e8
Amit <amit@industrybuying.com>
2017-05-28 23:08:23 +0530

    mysql inst changes

--- mysql-inst.sql
+++ mysql-inst.sql
@@ -1,18 +1,6 @@
 
 CREATE DATABASE `parking`;
 
-CREATE TABLE `parking_slip` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `v_num` varchar(255) NOT NULL,
-  `check_in` datetime NOT NULL,
-  `check_out` datetime DEFAULT NULL,
-  `amount` double DEFAULT NULL,
-  `state` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `vvv_num` (`v_num`),
-  CONSTRAINT `fk_v` FOREIGN KEY (`v_num`) REFERENCES `vehicle` (`v_num`)
-); 
-
  CREATE TABLE `charges` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `created_at` datetime NOT NULL,
@@ -29,3 +17,17 @@ CREATE TABLE `parking_slip` (
   `v_type` varchar(255) NOT NULL,
   PRIMARY KEY (`v_num`)
 );
+
+CREATE TABLE `parking_slip` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `v_num` varchar(255) NOT NULL,
+  `check_in` datetime NOT NULL,
+  `check_out` datetime DEFAULT NULL,
+  `amount` double DEFAULT NULL,
+  `state` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `vvv_num` (`v_num`),
+  CONSTRAINT `fk_v` FOREIGN KEY (`v_num`) REFERENCES `vehicle` (`v_num`)
+); 
+
+
